1d506094be70c41887fcc4720b965d717bbe7916
saurabh kumar <saurabhkumar9493@gmail.com>
2024-05-01 15:07:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './shilpa.jpeg';
+import Header from './Header';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  console.log("hello from reactjs")
+
+  return( 
+        <div>
+         
+         <Header/>
+         
+
+        <div>
+          <img src={img}/>
+          <img src={img}/>
+          <img src={img}/>
+          <img src={img}/>
+        </div>
+
+        <Footer/>
+        
+        </div>
+      
+        
+      
+  )
+           
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+
+
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className="social-icons">
+                    <a href="https://www.facebook.com"><FaFacebook /></a>
+                    <a href="https://www.twitter.com"><FaTwitter /></a>
+                    <a href="https://www.instagram.com"><FaInstagram /></a>
+                </div>
+                <p>&copy; 2024 Your Company Name. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
